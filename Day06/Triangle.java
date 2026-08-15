37e613254b7c54e93f4867477ef29998760a0c0f
Payal Bhadoriya <payalbhadoriya009@gmail.com>
2026-08-15 23:26:33 +0530

    Day 6: nested loops and patterns

---
+++ Day06/Triangle.java
@@ -0,0 +1,44 @@
+package Day06;
+
+public class Triangle {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+
+        for(int k = 5; k >= 1; k--) {
+
+            for(int l = 1; l <= k; l++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        for(int b = 1; b <= 5; b++) {
+
+            for(int a = 1; a <= b; a++){
+                System.out.print(a+ " ");
+            }
+            System.out.println();
+        }
+
+        for(int q = 1; q <= 5; q++) {
+
+            for(int w = 1; w <= q; w++) {
+                System.out.print(w+ "");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+        
